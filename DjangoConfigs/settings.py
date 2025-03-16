5ec3cf3ba8376d3aa7ab7d10eaa1c321dee83577
HenryXiaoYang <henryyang666@hotmail.com>
2025-03-16 14:05:39 +0800

    fix: DEBUG false

--- DjangoConfigs/settings.py
+++ DjangoConfigs/settings.py
@@ -27,7 +27,7 @@ if not os.environ.get('DJANGO_SECRET_KEY', None):
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = []
